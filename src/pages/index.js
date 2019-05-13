bdf37261256a2092b5683927faf79841aac8478a
Casey Heinemeyer <doulos1129@gmail.com>
2019-05-13 14:22:52 -0700

    landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import Layout from '../components/layout';
+import { Link } from 'gatsby';
+import landingStyles from '../components/landing.module.scss';
 
 const IndexPage = () => (
-  <Layout />
+
+  <div className={landingStyles.banner}>
+    <h1 className={landingStyles.bannerText}>JD's shoppe</h1>
+    <div className={landingStyles.bannerNav}>
+      <div className={landingStyles.buttonContainer}>
+        <Link to="/items" className={landingStyles.btn}>Items</Link>
+      </div>
+      <div className={landingStyles.buttonContainer}>
+        <Link to="/about" className={landingStyles.btn}>About</Link>
+      </div>
+      <div className={landingStyles.buttonContainer}>
+        <Link to="/contact" className={landingStyles.btn}>Contact</Link>
+      </div>
+    </div>
+  </div>
+
+
 );
 
 export default IndexPage;
