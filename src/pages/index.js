b3065c3fd93f81be21d4256e50418ea27fbc6abb
Casey Heinemeyer <doulos1129@gmail.com>
2019-05-14 20:01:06 -0700

    landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import {  FaFacebook, FaInstagram, FaGoogle } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 import landingStyles from '../components/landing.module.scss';
 
 const IndexPage = () => (
   <div>
-    <div className={landingStyles.icons}>
-      <FaFacebook />
-      <FaInstagram />
-      <FaGoogle />
-    </div>
-    <div className={landingStyles.banner}>
-      <h1 className={landingStyles.bannerText}>JD's shoppe</h1>
-      <div className={landingStyles.bannerNav}>
-        <div className={landingStyles.buttonContainer}>
-          <Link to="/items" className={landingStyles.btn}>Items</Link>
-        </div>
-        <div className={landingStyles.buttonContainer}>
-          <Link to="/about" className={landingStyles.btn}>About</Link>
-        </div>
-        <div className={landingStyles.buttonContainer}>
-          <Link to="/contact" className={landingStyles.btn}>Contact</Link>
+
+    <div className={landingStyles.content}>
+      <div className={landingStyles.icons}>
+        <a href="www.facebook.com"><FaFacebook /></a>
+        <a href="www.instagram.com"><FaInstagram /></a>
+        <a href="www.twitter.com"><FaTwitter /></a>
+      </div>
+      <div className={landingStyles.banner}>
+        <h1 className={landingStyles.bannerText}>JD's shoppe</h1>
+        <div className={landingStyles.bannerNav}>
+          <div className={landingStyles.buttonContainer}>
+            <Link to="/items" className={landingStyles.btn}>Items</Link>
+          </div>
+          <div className={landingStyles.buttonContainer}>
+            <Link to="/about" className={landingStyles.btn}>About</Link>
+          </div>
+          <div className={landingStyles.buttonContainer}>
+            <Link to="/contact" className={landingStyles.btn}>Contact</Link>
+          </div>
         </div>
       </div>
     </div>
