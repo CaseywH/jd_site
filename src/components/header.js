d0079a9f05d3a13ce0ef42224c6e5d56390114d8
Casey Heinemeyer <doulos1129@gmail.com>
2019-05-15 13:58:45 -0700

    mobile style

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
-
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 import headerStyles from './header.module.scss';
 
 const Header = () => {
@@ -15,25 +15,33 @@ const Header = () => {
   `);
   return (
     <header className={headerStyles.header}>
-      <h1>
-        <Link to="/" className={headerStyles.title}>{data.site.siteMetadata.title}</Link>
-      </h1>
-      <nav>
-        <ul className={headerStyles.navList}>
-          <li>
-            <Link to="/" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>home</Link>
-          </li>
-          <li>
-            <Link to="/items" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>items</Link>
-          </li>
-          <li>
-            <Link to="/about" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>about</Link>
-          </li>
-          <li>
-            <Link to="/contact" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={headerStyles.navLeft}>
+        <h1>
+          <Link to="/" className={headerStyles.title}>{data.site.siteMetadata.title}</Link>
+        </h1>
+        <nav>
+          <ul className={headerStyles.navList}>
+            <li>
+              <Link to="/" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>home</Link>
+            </li>
+            <li>
+              <Link to="/items" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>items</Link>
+            </li>
+            <li>
+              <Link to="/about" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>about</Link>
+            </li>
+            <li>
+              <Link to="/contact" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className={headerStyles.navRight}>
+        <a href="www.facebook.com"><FaFacebook /></a>
+        <a href="www.instagram.com"><FaInstagram /></a>
+        <a href="www.twitter.com"><FaTwitter /></a>
+      </div>
+
     </header>
   );
 };
