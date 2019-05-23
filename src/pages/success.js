6065665ab47fc5a0b056bddffb63b30f505b3832
Casey Heinemeyer <doulos1129@gmail.com>
2019-05-23 00:06:25 -0700

    success page

---
+++ src/pages/success.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+
+const Success = () => (
+  <Layout>
+    <h1>Success Page!</h1>
+  </Layout>
+);
+
+export default Success;
