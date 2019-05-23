3d289a2df3437f20b369456fae1a4e159d5bba78
Casey Heinemeyer <doulos1129@gmail.com>
2019-05-22 20:13:09 -0700

    new form

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 const contact = () => (
   <div>
-    <form
-      name="contact"
-      method="post"
-      data-netlify="true"
-    >
-      <input name="name" type="text" placeholder="your name" />
-      <button type="submit">Send</button>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+Your Name:
+          {' '}
+          <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+Your Email:
+          {' '}
+          <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+Message:
+          {' '}
+          <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   </div>
 );
