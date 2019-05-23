6c2d7776bb188a4cad05be7ce2cbe268ee20cfb7
Casey Heinemeyer <doulos1129@gmail.com>
2019-05-23 00:03:55 -0700

    bot field

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,6 +3,8 @@ import React from 'react';
 const contact = () => (
   <div>
     <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>
 Your Name:
