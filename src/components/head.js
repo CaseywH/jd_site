1567fdd4bd183f214a2bda0aa9ce3ed363b67a6e
Casey Heinemeyer <doulos1129@gmail.com>
2019-05-30 13:46:06 -0700

    favicon

--- src/components/head.js
+++ src/components/head.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
+import favicon from '../../static/favicon.ico';
+
 const Head = ({ title }) => {
   const data = useStaticQuery(graphql`
         query{
@@ -15,6 +17,9 @@ const Head = ({ title }) => {
   return (
     <Helmet
       title={`${title} | ${data.site.siteMetadata.title}`}
+      link={[
+        { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
+      ]}
     />
   );
 };
